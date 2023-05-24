df26ad4d3276984ef72b0e4cc11012d41598890d
Baris Algun <bbarisalgun@gmail.com>
2023-05-24 03:38:28 +0300

    Update UserRestController.java

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRestController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRestController.java
@@ -13,7 +13,7 @@ public class UserRestController {
 
         @PostMapping("/users/check_email")
     public String checkDuplicateEmail(@Param("email") String email){
-        return service.isEmailunique(email) ? "Ok":"Duplicated";
+        return service.isEmailunique(email) ? "OK":"Duplicated";
     }
 
 
